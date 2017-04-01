34ca9b3050dda70cc43fbeda4ba609849c149b4b
jgege <juhaszgege@gmail.com>
2017-04-01 19:39:42 +0100

    Delete db.local.php

--- config/db.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=hackathon_roomhunt_dev',
-    'username' => 'b116_dev',
-    'password' => 'fFrYcwTmnCThdCjL',
-    'charset' => 'utf8',
-];
