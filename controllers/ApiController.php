8816f273a4bdc42dbcae5f26dc08645ca83b20c2
Gergely Juhasz <juhaszgege@gmail.com>
2017-04-01 08:32:59 -0400

    clean up

---
+++ controllers/ApiController.php
@@ -0,0 +1,51 @@
+<?php namespace app\controllers;
+
+class ApiController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    /*public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }*/
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+        return [
+            'Hello' => 'MLH Prime!',
+        ];
+    }
+}
